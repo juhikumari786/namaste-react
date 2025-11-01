431468d04fcde42ef7fcec5d9ada8b945c5abc7b
anandkr01 <juhisingh7860@gmail.com>
2025-11-01 09:31:12 +0530

    Episode - 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -8,14 +11,14 @@ const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1"),
+    React.createElement("h1", {id: "child1", key: "h1"}, "I am h1"),
     React.createElement(
       // Sibling
       "h2",
-      {},
+      {id: "child2", key: "h2"},
       "I am h2"
     ),
   ])
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
